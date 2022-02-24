ba518042aa63e3a647ba7758f851e529ea92f0c0
Richard Perko <richardperko@outlook.com>
2022-02-23 20:31:00 -0700

    Deleted large portion of code base, added populateDisplay function, added cleaner eventlistener for number buttons.

--- script.js
+++ script.js
@@ -1,85 +1,37 @@
-const clearBtn = document.querySelector('#clrBtn');
-clearBtn.onclick = () => clearScreen();
+const numberButtons = document.querySelectorAll('.number');
+const display = document.querySelector('#output')
 
-let currentNum = 0;
-let totalNum = 0;
-let previousSign
-// get values from buttons
-function getValue(value){
-  // set the current input 
-  (currentNum === 0) ? (currentNum = value) : (currentNum = ("" + currentNum + value))
+let displayValue = "";
+let currentNumber = "";
+let operator = "";
 
-  //display currentNum
-  display(currentNum);
+numberButtons.forEach(button => {
+  button.addEventListener('click', populateDisplay)
+});
+
+function populateDisplay() {
+  (!displayValue) ? (displayValue = this.textContent) : (displayValue += this.textContent)
+  display.textContent = displayValue;
 }
 
-function getOperator(sign){
-  console.log("total",totalNum)
-  console.log("current", currentNum)
-  console.log(sign)
-  if (totalNum === 0){
-    totalNum = currentNum;
-    currentNum = 0;
-  } else {
-    currentNum = operate(totalNum, currentNum, previousSign)
-    totalNum = 0;
-    console.log(totalNum, "total")
-  }
-
-  if (sign === "="){
-    console.log("working?")
-  } else {
-    previousSign = sign;
-  }
-}
-
-function display(output){
-  document.getElementById('output').innerHTML = output;
-}
-
-function clearScreen(){
-  document.getElementById('output').innerHTML = "";
-  currentNum = 0;
-  totalNum = 0;
-}
-
-function addition(a, b){
-  totalNum = parseInt(a) + parseInt(b);
-  display(totalNum);
-  // currentNum = 0;
-  return(totalNum);
-}
-
-function subtraction(a, b){
-  totalNum = parseInt(a) - parseInt(b);
-  display(totalNum);
-  // currentNum = 0;
-  return(totalNum);
-}
-
-function mutliplication(a, b){
-  totalNum = parseInt(a) * parseInt(b);
-  display(totalNum);
-  // currentNum = 0;
-  return(totalNum);
-}
-
-function division(a, b){
-  totalNum = parseInt(a) / parseInt(b);
-  display(totalNum);
-  // currentNum = 0;
-  return(totalNum);
-}
 
 function operate(a, b, operator){
     switch (operator){
       case "+" :
-        return addition(a, b);
+        totalNum = parseInt(a) + parseInt(b);
+        display(totalNum);
+        return(totalNum);
       case "-" :
-        return subtraction(a, b);
+        totalNum = parseInt(a) - parseInt(b);
+        display(totalNum);
+        return(totalNum);
       case "*" : 
-        return mutliplication(a, b);
+      totalNum = parseInt(a) * parseInt(b);
+      display(totalNum);
+      return(totalNum);
       case "/" :
-        return division(a, b);
+        totalNum = parseInt(a) / parseInt(b);
+        display(totalNum);
+        return(totalNum);
     }
 }

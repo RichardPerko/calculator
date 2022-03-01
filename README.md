66765125daca5dcd13cab5b7d07da7cae6c6a4b0
Richard Perko <richardperko@outlook.com>
2022-02-28 18:59:23 -0700

    added css, added ripple effect, add header and links with images, added final touches, finish!

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Carl Perko
 16 Feburary 2022
 
 This is the calculator project for The Odin Project, It is the final project in the foundations course! I will be doing my absolute best to do as much of this project by myself. I really want to push myself to think about the problems I face and how to fix them. Excited to get started!
+
+28 Feburary 2022
+
+Another project down! Looking back there are a few things I did good and a few things I need to work on. Starting with the good, I spent a lot of time really working through each individual issue that happened. My main strategy was to walk away and explain the issue to a "teddy bear" often this lead me right to the answer I was looking for. This worked for awhile and I am really proud of what I was able to build. The issue I ran into was relying on weak conditionals and the overuse of 0 to verify variables. Admittly I had to take a look at how others approached this program. What I found when I looked was that I was extremely close, my logic was good but like I said the conditionals really made it confusing. I had to refactor my program to be alot more robust. My original operator function was doing a lot of the hard work which led to a few bugs that made me use the many conditionals. Overall this new version is alot more sturdy and I am happy to have completed it! If it is not obvious the CSS is designed after the IOS calculator.
--- script.js
+++ script.js
@@ -7,10 +7,37 @@ const posNegButton = document.querySelector('#posNeg');
 const decimalButton = document.querySelector('#decimal');
 const equalsButton = document.querySelector('#equals')
 
+const buttons = document.getElementsByTagName("button");
+for (const button of buttons){
+    button.addEventListener("click", createRipple);
+}
+
 let displayValue = "";
 let currentNumber = "";
-let operatorCurrent = "";
+let prevOperator = "";
 
+// performs calculations and displays them
+function operate(operator, a, b){
+  switch (operator){
+    case "add" :
+      display.textContent = a + b
+      break;
+    case "subtract" :
+      display.textContent = a - b
+      break;
+    case "multiply" : 
+      display.textContent = a * b
+      break;
+    case "divide" :
+      display.textContent = a / b
+      break;
+  }
+
+// stores current number and resets other
+  currentNumber = display.textContent;
+  prevOperator = "";
+  displayValue = "";
+}
 // adds events onto each number, when clicked called populateDisplay
 numberButtons.forEach(button => {
   button.addEventListener('click', populateDisplay)
@@ -18,6 +45,7 @@ numberButtons.forEach(button => {
 
 // populates display with the button press, adds additional presses to displayValue
 function populateDisplay() {
+  document.querySelector('#clear').innerHTML = "C";
   (!displayValue) ? (displayValue = this.textContent) : (displayValue += this.textContent)
   display.textContent = displayValue;
 }
@@ -25,11 +53,12 @@ function populateDisplay() {
 operators.forEach(button => {
   button.addEventListener('click', () => {
     // if a number and operator is stored operate
-    if(currentNumber && operatorCurrent){
-      operate(operatorCurrent, Number(currentNumber), Number(displayValue));
+    if(currentNumber && prevOperator){
+      operate(prevOperator, Number(currentNumber), Number(displayValue));
+      
     }
     // if two numbers are stored without an operator store the number and reset the previous
-    else if (!operatorCurrent && currentNumber && displayValue){
+    else if (!prevOperator && currentNumber && displayValue){
       currentNumber = displayValue;
       displayValue = "";
     }
@@ -43,37 +72,14 @@ operators.forEach(button => {
       displayValue = "";
     }
     // stores operator based on button press
-    operatorCurrent = button.id
+    prevOperator = button.id
   });
 });
 
 // call the operate function
 equalsButton.addEventListener('click', () => {
-  operate(operatorCurrent, Number(currentNumber), Number(displayValue));
+  operate(prevOperator, Number(currentNumber), Number(displayValue));
 })
-
-// performs calculations and displays them
-function operate(operator, a, b){
-    switch (operator){
-      case "add" :
-        display.textContent = a + b
-        break;
-      case "subtract" :
-        display.textContent = a - b
-        break;
-      case "multiply" : 
-        display.textContent = a * b
-        break;
-      case "divide" :
-        display.textContent = a / b
-        break;
-    }
-
-    // stores current number and resets other
-    currentNumber = display.textContent;
-    operatorCurrent = "";
-    displayValue = "";
-  }
 
   // if there is a display value make it negative, if number is stored in currentNumber make it negative
 posNegButton.addEventListener('click', () => {
@@ -111,5 +117,28 @@ clearButton.addEventListener('click', () =>{
   display.innerText = 0;
   displayValue = "";
   currentNumber = "";
-  operatorCurrent = "";
+  prevOperator = "";
+  document.querySelector('#clear').innerHTML = "AC";
 });
+
+// ripple on click function from css tricks
+function createRipple(event) {
+  const button = event.currentTarget;
+
+  const circle = document.createElement("span");
+  const diameter = Math.max(button.clientWidth, button.clientHeight);
+  const radius = diameter / 2;
+
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+  circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+  circle.classList.add("ripple");
+
+  const ripple = button.getElementsByClassName("ripple")[0];
+
+  if (ripple) {
+    ripple.remove();
+  }
+
+  button.appendChild(circle);
+}
